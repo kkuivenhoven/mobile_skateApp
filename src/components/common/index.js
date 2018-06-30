d0404bffa266a8982086329dd679c8cdb7bfe73c
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2018-06-30 12:51:01 -0700

    added common components from tutorial

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+export * from './Button.js';
+export * from './Card.js';
+export * from './CardSection.js';
+export * from './Header.js';
+export * from './Input.js';
+export * from './Spinner.js';
+export * from './Confirm.js';
+export * from './Zip.js';
+
